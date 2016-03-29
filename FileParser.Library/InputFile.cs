e346423935582297790e0327416eca0f09096697
Admir Alicic <Admir Alicic>
2016-03-29 12:03:14 +0200

    Created Main Form

---
+++ FileParser.Library/InputFile.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileParser.Library
+{
+    public class InputFile
+    {
+        private string _filePath;
+
+        public InputFile(string filePath)
+        {
+            try
+            {
+                var path = Path.GetFullPath(filePath);
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException("File path invalid");
+            }
+
+            var fileExtension = Path.GetExtension(filePath);
+            
+            if (!fileExtension.Equals(".json") && !fileExtension.Equals(".csv"))
+                throw new ArgumentException("Input file must be 'json' or 'csv'");
+
+            _filePath = filePath;
+        }
+
+        public string GetFilePath()
+        {
+            return _filePath;
+        }
+
+        public object GetFileName()
+        {
+            return Path.GetFileNameWithoutExtension(_filePath);
+        }
+
+        public object GetFileExtension()
+        {
+            return Path.GetExtension(_filePath);
+        }
+    }
+}
---
+++ FileParser.UI/MainForm.cs
@@ -0,0 +1,29 @@
+﻿using FileParser.Library;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileParser.UI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            var inputFile = new InputFile(@"C:/Tests/inputA.csv");
+
+            var fileName = inputFile.GetFileName();
+            var fileExtension = inputFile.GetFileExtension();
+        }
+    }
+}
